b63895cc3b37d9feca312313d1737afa60b10a38
Varian Avila Faldi <varian236@gmail.com>
2022-05-18 11:33:07 +0700

    order

--- admin/manage-orders.php
+++ admin/manage-orders.php
@@ -1,42 +1,69 @@
 <?php include('partials/header.php'); ?>
         <!-- main START -->
-        <section class="manage-admin" style="min-height: 50%;">
+        <section class="admin manage-orders">
             <div class="container">
                 <div class="row mb-1">
-                     <div class="col mt-5">
+                    <div class="col mt-5">
                           <h3 class="text-dark">Manage Orders</h3>
+                    </div>
                 </div>
-                <div class="addAdmin">
-                    <a href="#" class="btn btn-primary mb-2">Add Admin</a>
-                </div>
+                <?php
+                   if(isset($_SESSION['update'])){
+                        echo '
+                            <div class="row mb-1">
+                                <div class="col-md-12">
+                                    <h4 class="text-success fs-5"><em>'.$_SESSION['update'].'</em></h4>
+                                </div>
+                            </div>
+                            ';
+                        unset($_SESSION['update']);
+                    }
+                ?>
                 <table class="table">
                     <thead class="table-dark">
                         <tr>
-                        <th scope="col">ID</th>
-                        <th scope="col">Username</th>
-                        <th scope="col">Password</th>
+                        <th scope="col">No</th>
+                        <th scope="col">Nama Pemesan</th>
+                        <th scope="col">Tanggal</th>
+                        <th scope="col">Status</th>
                         <th scope="col">Action</th>
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <th scope="row">1</th>
-                            <td>admin</td>
-                            <td>admin</td>
-                            <td>
-                                <a href="#" class="btn btn-primary btn-sm btn-success">Edit Admin</a>
-                                <a href="#" class="btn btn-primary btn-sm btn-danger">Delete Admin</a>
-                            </td>
-                        </tr>
-                        <tr>
-                            <th scope="row">2</th>
-                            <td>admin</td>
-                            <td>admin</td>
-                            <td>
-                                <a href="#" class="btn btn-primary btn-sm btn-success">Edit Admin</a>
-                                <a href="#" class="btn btn-primary btn-sm btn-danger">Delete Admin</a>
-                            </td>
-                        </tr>
+                        <?php
+                            //Select data admin dari database
+                            $sql = "SELECT * from kustomer k inner join keranjang on k.id_kustomer = keranjang.id_kustomer inner join orders on keranjang.id_keranjang = orders.id_keranjang";
+
+                            //Eksekusi Query
+                            $result = mysqli_query($conn, $sql) or die("Query gagal dijalankan: " . mysqli_errno($conn));
+                            //Cek query
+                            if($result){
+                                //hitung banyak baris
+                                $row = mysqli_num_rows($result);
+
+                                //Jika ada data
+                                if($row > 0){
+                                    $number = 1;
+                                    //Looping data
+                                    while($data = mysqli_fetch_assoc($result)){
+                                        echo '
+                                            <tr>
+                                                <td scope="row">'.$data['id_orders'].'</td>
+                                                <td>'.$data['nama'].'</td>
+                                                <td>'.$data['tgl'].'</td>
+                                                <td>'.$data['status'].'</td>
+                                                <td>
+                                                    <a href="update-orders.php?id='.$data['id_orders'].'&nama='.$data['nama'].'&tgl='.$data['tgl'].'" class="btn btn-primary btn-sm btn-success">Edit Admin</a>
+                                                    <a href="delete-admin.php?id='.$data['id_orders'].'" class="btn btn-primary btn-sm btn-danger">Delete Admin</a>
+                                                </td>
+                                            </tr>';}
+                                }else{
+                                    echo '<tr>';
+                                    echo '<td colspan="4" class="table-danger">Data tidak ditemukan!</td>';
+                                    echo '</tr>';
+                                }
+                            }
+                        ?>
                     </tbody>
                 </table>
             </div>
